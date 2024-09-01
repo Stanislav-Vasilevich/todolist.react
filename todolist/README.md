9c7159d130e0e25015693bf3e281b27eb047cf0c
StanislaVailevich <stanislavasilevich@yandex.ru>
2024-09-01 19:42:40 +0300

    Создал новый проект под todolist

---
+++ todolist/README.md
@@ -0,0 +1,14 @@
+# Todolist
+
+Перейти в основной [рабочий проект](https://github.com/facebook/create-react-app)
+
+## Модули
+
+Установка npm-пакетов: 
+### `yarn`
+
+Запуск:
+### `yarn start`
+
+Сборка:
+### `yarn build`
---
+++ todolist/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+
+root.render(
+	<React.StrictMode>
+		<App/>
+	</React.StrictMode>
+);
+
+/* <React.StrictMode> — это инструмент в React, который активирует дополнительные проверки и предупреждения для обнаружения потенциальных проблем в вашем приложении. Он помогает выявить устаревший код, небезопасные методы жизненного цикла и неожиданные побочные эффекты. Строгий режим работает только в режиме разработки и не влияет на сборку. */
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
